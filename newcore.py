440b92636546d42cad551c618d5a88667cb738a2
lachlanc61 <lachlanc.612@gmail.com>
2022-08-10 23:23:47 +1000

    merged avi and fft into main fit, not debugged yet

--- newcore.py
+++ newcore.py
@@ -27,6 +27,11 @@ wdirname='data'     #working directory relative to script
 odirname='out'      #output directory relative to script
 infile = "as_amC_stable.avi"
 
+#properties of input file
+pxpitch=1.252       #nm per pixel in real space
+pxdim=512           #width of RS image (should really calc this directly...)
+etime=0.1           #seconds per frame
+
 #figure params
 colourmap='Set1'    #colourmap for figure
 #colourmap='copper'    #colourmap for figure
@@ -37,6 +42,30 @@ medfont = 10
 lgfont = 12
 lwidth = 1  #default linewidth
 bwidth = 1  #default border width
+
+#radial params
+centrecut=5     #minimum radius (centre mask)
+secwidth=45     #width of sector
+secstep=45      #step between sectors
+
+#initial guesses for fit function
+amp=20          #amplitude
+Cs=2.7E6        #spherical aberration coeff, nm (=2.7 mm)
+wl=0.00335      #wavelength (nm) 
+    #from accel voltage via de broglie eqn eg. https://www.ou.edu/research/electron/bmz5364/calc-kv.html
+dz=27500        #defocus value (depth of field)  #FIT THIS
+dm=130          #damping param
+dec=20          #decay param
+c=30            #constant
+
+gsig=0.035  #gaussian sigma
+gamp=40     #gaussian amplitude
+
+#fit bounding params
+bf3=99      #very free
+bf2=2       #constrained     
+bf1=1.3     #highly constrained
+bf0=1.01    #effectively fixed
 
 
 #-------------------------------------
@@ -58,10 +87,99 @@ def find_symmetric_coordinates(coords, centre):
     return (centre + (centre - coords[0]),
             centre + (centre - coords[1]))
 
+#rotate image
+#not used currently - alternative to rotating mask
+def rotate_image(image, angle):
+  image_center = tuple(np.array(image.shape[1::-1]) / 2)
+  rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+  result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+  return result
 
+#get index from array closest to value
+#https://stackoverflow.com/questions/2566412/find-nearest-value-in-numpy-array
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return idx
+
+#get index where component first crosses axis
+#https://stackoverflow.com/questions/3843017/efficiently-detect-sign-changes-in-python
+def zerocross(array):
+    return np.where(np.diff(np.sign(array)))[0]
+
+#Generate a radial profile
+def radial_profile(data, center):
+#    print(data[center[0],:])
+#    print(np.indices((data.shape)))
+    y,x = np.indices((data.shape)) # first determine radii of all pixels
+    r = np.sqrt((x-center[0])**2+(y-center[1])**2)
+   # print(r)
+    ind = np.argsort(r.flat) # get sorted indices
+    sr = r.flat[ind] # sorted radii
+    sim = data.flat[ind] # image values sorted by radii
+    ri = sr.astype(np.int32) # integer part of radii (bin size = 1)
+    # determining distance between changes
+    deltar = ri[1:] - ri[:-1] # assume all radii represented
+    rind = np.where(deltar)[0] # location of changed radius
+    nr = rind[1:] - rind[:-1] # number in radius bin
+    csim = np.cumsum(sim, dtype=np.float64) # cumulative sum to figure out sums for each radii bin
+    tbin = csim[rind[1:]] - csim[rind[:-1]] # sum for image values in radius bins
+    radialprofile = tbin/nr # the answer
+    return radialprofile
+
+#Create a radial sector mask
+#based on https://stackoverflow.com/questions/59432324/how-to-mask-image-with-binary-mask
+def sector_mask(shape,centre,radius,angle_range):
+    """
+    Return a boolean mask for a circular sector. The start/stop angles in  
+    `angle_range` should be given in clockwise order.
+    """
+
+    x,y = np.ogrid[:shape[0],:shape[1]]
+    cx,cy = centre
+    tmin,tmax = np.deg2rad(angle_range)
+
+    # convert cartesian --> polar coordinates
+    r2 = (x-cx)*(x-cx) + (y-cy)*(y-cy)
+    theta = -1*np.arctan2(x-cx,y-cy)
+    
+    # wrap angles between 0 and 2*pi
+    theta %= (2*np.pi)
+
+    # circular mask
+    circmask = r2 <= radius*radius
+
+    # angular mask
+    if (tmin <= 0) and (tmax >= 0):
+        #or 360
+        tmin = 2*np.pi+tmin
+        anglemask = np.logical_and(theta >= tmin,theta <= 2*np.pi) #or (theta <= (tmax+np.pi))
+        anglemask += np.logical_and(theta >= 0,theta <= tmax)
+    elif (tmin <= 2*np.pi) and (tmax >= 2*np.pi):
+        tmax = tmax-2*np.pi
+        anglemask = np.logical_and(theta >= tmin,theta <= 2*np.pi) #or (theta <= (tmax+np.pi))
+        anglemask += np.logical_and(theta >= 0,theta <= tmax)
+    else:
+        anglemask = np.logical_and(theta >= tmin%(2*np.pi),theta <= tmax%(2*np.pi)) #or (theta <= (tmax+np.pi))
+    
+    return circmask*anglemask
+
+#outputs gaussian with max y = amp
+def ngauss(x, mu, sig1, amp):
+    g1=norm.pdf(x, mu, sig1)
+    g1n=np.divide(g1,max(g1))
+    return np.multiply(g1n, amp)
+
+
+def ctfmodel(x, amp, Cs, wl, dz, dm, dec, c, gsig, gamp):
+    y = np.zeros_like(x)
+#    y=abs(-2*amp*(1/(x**dm))*(np.sin( (np.pi/2)*(Cs*wl**3*x**4 - 2*dz*wl*x**2))))-dec*x+c
+#    print(-dm*x**2, np.exp(-dm*x**2))
+    y=ngauss(x, 0, gsig, gamp)+2*amp*(np.exp(-dm*x**2))*abs(np.sin( (np.pi/2)*(Cs*wl**3*x**4 - 2*dz*wl*x**2)))-dec*x+c
+    return y
 
 #-----------------------------------
-#MAIN START
+#INITIALISE
 #-----------------------------------
 
 #initialise directories relative to script
@@ -84,56 +202,233 @@ plt.rc('figure', titlesize=lgfont)  # fontsize of the figure title
 plt.rc('lines', linewidth=lwidth)
 plt.rcParams['axes.linewidth'] = bwidth
 
-
-
-
-
 f = os.path.join(wdir,infile)
-
 fname = os.path.splitext(os.path.basename(f))[0]
-print("opening ",fname)
+
+#-----------------------------------
+#MAIN START
+#-----------------------------------
+
 vidcap = cv2.VideoCapture(f)
-success,image = vidcap.read()
+
+#get total number of frames in avi (maybe different in more recent version of cv2?)
+#https://stackoverflow.com/questions/25359288/how-to-know-total-number-of-frame-in-a-file-with-cv2-in-python
+vidlen = int(cv2.VideoCapture.get(vidcap, int(cv2.CAP_PROP_FRAME_COUNT)))
+
+print("opening ",fname)
+print("no frames:", vidlen)
+
+#initalise result arrays
+times= np.empty(vidlen, dtype="U10")
+zavg=np.zeros(vidlen)    #zero point average
+zvar=np.zeros(vidlen)   #zero point variance
+r2avg=np.zeros(vidlen)  #average r2 
+
+#initialise tracking vars
 framecount = 0
+success = True
+
+#loop through all frames
 while success:
-    #cv2.imwrite(os.path.join(odir, "frame%d.tif" % count),image)     # save frame as TIF file      
+
+    #initialise plot and colourmaps per frame
+    
+    plt.rcParams["figure.figsize"] = [figx/2.54, figy/2.54]
+    plt.rcParams["figure.figsize"] = [figx/2.54, figy/2.54]
+    fig=plt.figure()
+
+    steps=np.arange(0, 180, secstep)    #no. steps for radial masks
+    lut = cm = plt.get_cmap(colourmap) 
+    cNorm  = colors.Normalize(vmin=0, vmax=len(steps)+2)
+    scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=lut)
+
+    #initialise frame plot
+    axgph=fig.add_axes([0.08,0.1,0.85,0.4])
+
+    #read in frame
     success,rsimage = vidcap.read()
-    rsimage = rsimage[:, :, :3].mean(axis=2)  # Convert to grayscale
+
+    #leave loop if import unsuccessful (ie. no more frames)
+    print('Read frame : ', framecount, success)
+    if not success:
+        break
+  
+    # Convert to grayscale
+    rsimage = rsimage[:, :, :3].mean(axis=2)  
+
     # Array dimensions (array is square) and centre pixel
-    # Use smallest of the dimensions and ensure it's odd
+    # Use smallest dimension and ensure value is odd
     array_size = min(rsimage.shape) - 1 + min(rsimage.shape) % 2
-    # Crop image so it's a square image
+
+    # Crop image to square
     rsimage = rsimage[:array_size, :array_size]
     centre = int((array_size - 1) / 2)
+
     # Get all coordinate pairs in the left half of the array,
     # including the column at the centre of the array (which
     # includes the centre pixel)
     coords_left_half = (
         (x, y) for x in range(array_size) for y in range(centre+1)
     )
+
     # Sort points based on distance from centre
     coords_left_half = sorted(
         coords_left_half,
         key=lambda x: calculate_distance_from_centre(x, centre)
     )
 
-    plt.set_cmap("gray")
+    # fourier transform this frame
     ftimage = calculate_2dft(rsimage)
-
+    print(abs(ftimage[0,:10]))
+    """"
  # Show grayscale image and its Fourier transform
+    plt.set_cmap("gray")
     plt.subplot(121)
     plt.imshow(rsimage)
     plt.axis("off")
     plt.subplot(122)
-    print(ftimage)
-    print(np.abs(ftimage))
+#    print(ftimage)
+#    print(np.abs(ftimage))
 #    cv2.imwrite(os.path.join(odir, "fft_%s.tif" % fname),np.abs(ft))
     plt.imshow(np.log(abs(ftimage)))
     plt.axis("off")
     plt.pause(2)
 
     plt.show()
+    """
+# get image centres, outer radius
+    centrex, centrey = tuple(np.array(ftimage.shape[1::-1]) / 2)
+    centrex=int(centrex)
+    centrey=int(centrey)
+    radcut=int(max(centrex, centrey)*0.9)
+    rpoints=radcut-centrecut-1
+    
+    #initialise the profile array
+    profiles=np.zeros((len(steps),rpoints,2))
+    ctfs=np.zeros((len(steps),rpoints))
+    zvals=np.zeros(len(steps))
+    r2=np.zeros(len(steps))
 
-    print('Read frame : ', framecount, success)
+    stepcount=0 #stepcounter
+#   create series of radial masks
+#       iterate through each mask position according to secwith and secstep
+    for secmid in steps:
+
+        #duplicate the image
+        img = np.copy(ftimage)
+        colorVal = scalarMap.to_rgba(stepcount)
+        #initialise mask from sector coords
+        th1=secmid-secwidth/2
+        th2=secmid+secwidth/2
+        #generate the paired masks
+        mask = sector_mask(img.shape,(centrex,centrey),radcut,(th1,th2))
+        mask += sector_mask(img.shape,(centrex,centrey),radcut,(th1+180,th2+180))
+        #add masks on centre xy column/rows
+        mask[:,centrey] = False
+        mask[centrex,:] = False
+        #apply mask               
+        img[~mask] = 0
+
+
+
+    #   get the centre and centremask
+        center, ccut = (centrex, centrey), centrecut
+        
+    #   create the azimuthal profile (x,rad) and add to master matrix for this image
+        rad = radial_profile(img, center)
+        x = np.arange(rad.shape[0])
+        rad=rad[ccut:(radcut)]
+        x=x[ccut:(radcut)]
+        rad=rad[:rpoints]
+        x=x[:rpoints]
+        profiles[stepcount,:,:] = np.c_[x, rad]
+
+    #   https://stackoverflow.com/questions/22895794/scipys-optimize-curve-fit-limits
+    #   FITTING HERE
+    #--------------------------------------------------------
+        # k=x
+        k=x/(pxpitch*pxdim)  
+        
+        guess=np.array([amp, Cs, wl, dz, dm, dec, c, gsig, gamp])
+
+        bounded=([amp/bf3, Cs/(bf0), wl/bf0, dz/bf2, dm/bf2, dec/bf3, c/bf3, gsig/bf2, gamp/bf2], [amp*bf2, Cs*bf0, wl*bf0, dz*bf2, dm*bf2, dec*bf2, c*bf1, gsig*bf2, gamp*bf2])
+
+    #   DO FIT 
+        popt, pcov = curve_fit(ctfmodel, k, rad, p0=guess, bounds=bounded)
+    #--------------------------------------------------------
+
+        #populate final models
+        ctf=ctfmodel(k, *popt)
+        ctfs[stepcount,:]=ctf
+
+        #create model for sin component
+        sinfac=np.sin( (np.pi/2)*(popt[1]*popt[2]**3*k**4 - 2*popt[3]*popt[2]*k**2))
+
+        #get index of first crossing of x-axis -> this is the zero point
+        zpoint=zerocross(sinfac)[0]
+        zvals[stepcount]=k[zpoint]
+
+    #   calc r2 value as rough goodness-of-fit
+
+        # residual sum of squares
+        ss_res = np.sum((rad - ctf) ** 2)
+
+        # total sum of squares
+        ss_tot = np.sum((rad - np.mean(rad)) ** 2)
+
+        # r-squared
+        r2[stepcount] = 1 - (ss_res / ss_tot)
+
+        #   PLOTS
+        #plot data, fits, zeropoint
+
+        axrad=fig.add_axes([0.08+0.217*stepcount,0.52,0.20,0.45])
+        axrad.spines[:].set_linewidth(2)
+        axrad.spines[:].set_color(colorVal)
+        axrad.set_xticklabels([])
+        axrad.set_yticklabels([])
+        axrad.tick_params(color=colorVal, labelcolor=colorVal)
+        axrad.imshow(img)
+        axgph.plot(k, rad,
+            label="%d deg" % secmid,
+            color=colorVal)
+        axgph.plot(k, ctf, 
+            ':',
+            color=colorVal)
+
+        axgph.axvline(x=k[zpoint], color=colorVal, linestyle='--')    
+        axgph.text(k[zpoint]*1.05,0.95*max(ctf),
+            ("%.3f $nm^{-1}$" % k[zpoint]),
+            horizontalalignment='left',
+            color=colorVal)
+        stepcount += 1   #increment stepcounter
+
+    #FINAL PLOT per tiff
+
+    #adjust labels, legends etc    
+    axgph.set_ylabel('Intensity')
+    axgph.set_xlabel('Spatial frequency (1/nm)')
+    axgph.legend(loc="upper right")
+
+    #output the final figure for this file
+    fig.savefig(os.path.join(odir, ("out_%s.png" % fname)), dpi=300)
+    
+    #add stats to output matrices
+    times[framecount]=etime*framecount
+    zavg[framecount]=np.average(zvals)
+    zvar[framecount]=np.var(zvals)
+    r2avg[framecount]=np.average(r2)
+#   would be useful to report difference between sector ctfs as well
+#       could re-use variance code removed last commit
+
+#   clear and close the figure
+#       note: this is slow, costs about 20% performance
+#       ideally clear individual axes instead eg. axgh.cla() axrad.cla()
+#       some kind of bug with this right now
+    fig.clf()
+    plt.close()
+     
     framecount += 1
-    exit()
+
+
+print("CLEAN END")
